f554dee5f15e9c8e4340e0157260b287aa636c92
wuhao <wuhao@jkzl.com>
2017-01-11 18:39:41 +0800

    mv payChannel to bizChannel; add announcement file

--- app/controllers/conf/ConfController.scala
+++ app/controllers/conf/ConfController.scala
@@ -10,11 +10,26 @@ import models.conf.BizChannel
 import controllers._
 
 object ConfController extends Controller {
-	def index= Action {
-		Ok(views.html.conf.payChannel())
+	def index = Action {
+		Ok(views.html.conf.bizChannel())
 	}
 
-	def listPayChannel(limit: Int, offset: Int, keyword: String) = Action.async { request =>
+	def bizChannelMgr = Action {
+		Ok(views.html.conf.bizChannel())
+	}
+
+	def listBizChannel(limit: Int, offset: Int, keyword: String) = Action.async { request =>
+		BizChannel.list(limit, offset, keyword) map {
+			case channels:BizChannelArray => Ok(Json.toJson(channels))
+			case _ => NoContent
+		}
+	}
+
+	def announcementMgr = Action {
+		Ok(views.html.conf.announcement())
+	}
+
+	def listAnnouncement(limit: Int, offset: Int, keyword: String) = Action.async { request =>
 		BizChannel.list(limit, offset, keyword) map {
 			case channels:BizChannelArray => Ok(Json.toJson(channels))
 			case _ => NoContent
